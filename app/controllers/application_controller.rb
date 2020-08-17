16731ffd592fd80a90dac521db1154b3b1d46b1d
Tomasz Trzos <ttrzos@inpost.pl>
2020-08-17 23:11:00 +0200

    Setup root in application index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  def index
+    render(plain: "CastleApp by Tomasz Trzos. Environment: #{Rails.env}")
+  end
 end
