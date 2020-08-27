f97cb11bd782f7c0cd10ccbc41a6c81910a92b49
Tomasz Trzos <trzos.tomasz@gmail.com>
2020-08-27 23:23:35 +0200

    Add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+organization = Organization.create(
+  name: 'CastleIo'
+)
+
+IpBanSettingsSet.create(
+  organization_id: organization.id,
+  ban_duration: 10,
+  number_of_permitted_failed_requests: 5,
+  check_duration: 10,
+  number_of_emails_within_check: 5,
+)
